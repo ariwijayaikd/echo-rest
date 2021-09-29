60765f3863f1daaa71b5acd672a5d979acf03dc3
ariwijayaikd <ikadekariwijaya1@gmail.com>
2021-09-29 18:23:39 +0700

    penyesuaian

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module echo-rest
 go 1.17
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/labstack/echo/v4 v4.6.1 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
--- main.go
+++ main.go
@@ -1,51 +1,76 @@
-// Create New Simple CRUD
-// Entity details:
-// Table user:
-// id: int
-// username string
-// email string
-// roles: int
-
-// Create REST API:
-// Create new user: POST /user/
-// Update user: PUT /user/{id}
-// Delete user: DELETE /user/{id}
-// Get list users: GET /user
-// Get Detail user: GET /user/{id}
-
 package main
 
 import (
+	"fmt"
+	// "math/rand"
+	// "time"
 	"net/http"
-	"strconv"
+	// "strconv"
+	// "log"
+
+  // "github.com/jinzhu/gorm"
+	// _ "github.com/jinzhu/gorm/dialects/mysql"
+
 	"github.com/labstack/echo/v4"
+	// _ "github.com/go-sql-driver/mysql"
 )
+
+// var db *gorm.DB
+// var err error
+
+type User struct {
+	Id       int    `json:"id"`
+	Nickname string `json:"nickname"`
+	Emails   string `"json:email"`
+	Roles    int `json:"role"`
+}
+
+type Users []User
+
+var users Users
+
+// func generateId() int {
+// 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+// 	return r.Intn(10000)
+// }
 
 func listUser(c echo.Context) error {
 	// User ID from path `users/:id`
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
+	return c.JSON(http.StatusOK, users)
 }
 
 func newUser(c echo.Context) error {
-	// Get name and email
-	name := c.FormValue("name")
-	email := c.FormValue("email")
-	roles := c.FormValue("roles")
-	return c.String(http.StatusOK, "name:" + name + ", email:" + email + ", roles:" + roles)
+	user := User{}
+	err := c.Bind(&user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnprocessableEntity)
+	}
+	users = append(users, user)
+	return c.JSON(http.StatusCreated, users)
 }
 
 func main() {
+	fmt.Println("Running...")
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, welcome to echo!")
 	})
+
+	// db, err := gorm.Open("mysql", "root:Gebangsari1@/go_rest_api_crud?charset=utf8&parseTime=True")
+	// if err != nil {
+	// 	log.Println("Connection failed", err)
+	// } else {
+	// 	log.Println("Connection enstabilshed")
+	// }
+	//
+	// db.AutoMigrate(&Product{})
+	// handleRequests()
 
 	e.POST("/user", newUser)
 	e.GET("/user/:id", listUser)
-	e.GET("/user", detailUser)
-	e.PUT("/user/:id", updateUser)
-	e.DELETE("/user/:id", deleteUser)
+	// e.GET("/user", detailUser)
+	// e.PUT("/user/:id", updateUser)
+	// e.DELETE("/user/:id", deleteUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
